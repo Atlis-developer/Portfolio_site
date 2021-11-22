85d62225fa4605fb93c1f1c4357c787f4da21c5c
Atlis-Developer <utts.kyiv@gmail.com>
2021-11-22 18:06:05 +0200

    add hamburger

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import './Header.sass';
 
 export const Header = () => {
-    return (
-      <div className="header">
+
+  const [burger, setBurger] = useState(false)
+
+  return (
+    <div className="header">
+      <div className='header-laptop'>
         <a href="#general">Главная</a>
         <a href="#about_me">О себе</a>
         <a href="#skills">Навыки</a>
         <a href="#portfolio">Портфолио</a>
         <a href="#contacts">Контакты</a>
       </div>
-    );
-  }
+      <div className='header-mobile'>
+        <div className="menu" onClick={()=>setBurger(!burger)}>
+          <div className={burger ? 'hamburger-menu animate' : 'hamburger-menu'}></div>
+        </div>
+        {burger ? <div className='nav'>
+          <a href="#general">Главная</a>
+          <a href="#about_me">О себе</a>
+          <a href="#skills">Навыки</a>
+          <a href="#portfolio">Портфолио</a>
+          <a href="#contacts">Контакты</a>
+        </div> : null}
+      </div>
+    </div>
+  );
+}
